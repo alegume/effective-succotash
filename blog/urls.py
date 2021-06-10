822ffb693f2c3e3356254546b3af75b5e530aefa
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2021-06-10 20:11:42 -0300

    bootstrap bumped to 5 and icons added

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<pk>/', views.post_detail, name='post_detail'),
+    path('author/<username>/', views.author_perfil, name='author_perfil'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 from .models import Post
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    users = User.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts, 'users': users})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def author_perfil(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author=author)
+
+    return render(request, 'blog/author_perfil.html', {'author': author, 'posts': posts})
