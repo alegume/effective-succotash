21b4268edd09301f8b5549b10308238b2fa29522
Alexandre Abreu <alexandregrandeabreu@gmail.com>
2021-06-11 15:00:18 -0300

    first tutorial completed

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/new/', views.post_new, name='post_new'),
     path('author/<username>/', views.author_perfil, name='author_perfil'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
+from .forms import PostForm
 
 
 def post_list(request):
@@ -10,13 +12,45 @@ def post_list(request):
     users = User.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts, 'users': users})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def author_perfil(request, username):
     author = get_object_or_404(User, username=username)
     posts = Post.objects.filter(author=author)
 
     return render(request, 'blog/author_perfil.html', {'author': author, 'posts': posts})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
